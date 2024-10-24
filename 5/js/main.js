e27282e4e8dcf68cdabfdfa3eccf12001e5da885
Keks <keks@htmlacademy.ru>
2024-10-24 14:15:53 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,8 @@
+import { createPhotosArray } from './create-photos.js';
+
+const countPhotos = 25;
+
+const descriptionsPhotos = createPhotosArray(countPhotos);
+
+// eslint-disable-next-line no-console
+console.log(descriptionsPhotos);
