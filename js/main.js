5aea133f268cc3362ea7366a923e4d4b0b0c4907
Anastasia <ananasya777@gmail.com>
2024-11-14 00:48:00 +0500

    renders photos of other users

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import { createPhotosArray } from './create-photos.js';
+import { renderThumbnails } from './rendering-thumbnails.js';
 
 const countPhotos = 25;
 
 const descriptionsPhotos = createPhotosArray(countPhotos);
-
+renderThumbnails(descriptionsPhotos);
 // eslint-disable-next-line no-console
 console.log(descriptionsPhotos);
